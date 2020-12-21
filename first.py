b7435ba2197f68c7dbd935f08c3a0947bcded914
minseo <readex11@skuniv.ac.kr>
2020-12-22 04:54:22 +0900

    add folder

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print('Hello World')
+class Person:
+    def __init__(self,name,address):
+        self.name=name
+        self.address=address
+    
+    def greeting(self):
+        print('저는 {}입니다.'.format(self.name))
+
+minseo=Person('minseo','Seoul')
+minseo.greeting() 
